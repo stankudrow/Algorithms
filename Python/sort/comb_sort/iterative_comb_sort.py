75d43205b66fb9b56e8ef345851f0c20a3286516
Stanislav <staniskudriavtsev@yandex.ru>
2021-08-06 16:34:54 +0300

    comb sort added

---
+++ Python/sort/comb_sort/iterative_comb_sort.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+The iterative comb sort algorithm.
+
+The comb sort algorithm was:
+    * designed by Włodzimierz Dobosiewicz and Artur Borowy in 1980;
+    * rediscovered and named by Stephen Lacey and Richard Box in 1991.
+
+Notes
+-----
+    The comb sort is a generalisation of the bubble sort (1-gap) algorithm.
+
+References
+----------
+    * https://en.wikipedia.org/wiki/Comb_sort
+    * https://www.geeksforgeeks.org/comb-sort/
+    * https://www.tutorialspoint.com/Comb-Sort
+"""
+
+
+__author__ = "Stanislav D. Kudriavtsev"
+
+
+from typing import List, Sequence
+
+
+# Complexity: worst case
+# Time      : O(n**2) (simple case of gap choice)
+# Space:    : O(1) -> this implementation requires O(n)
+
+
+def iter_comb_sort(seq: Sequence) -> List:
+    """
+    Sort a sequence with the iterative comb sort algorithm.
+
+    Parameters
+    ----------
+    seq : Sequence
+
+    Returns
+    -------
+    List
+
+    """
+    lst = list(seq)  # copy -> purity sake
+    size = len(lst)
+    gap = size
+    shrink = 1.3
+    tosort = True
+    while (gap != 1) or tosort:
+        gap = max(int(gap / shrink), 1)
+        tosort = False
+        for i in range(size - gap):
+            j = i + gap
+            if lst[i] > lst[j]:
+                lst[i], lst[j] = lst[j], lst[i]
+                tosort = True
+    return lst
---
+++ Python/sort/comb_sort/test_comb_sort.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""The comb sort test module."""
+
+
+__author__ = "Stanislav D. Kudriavtsev"
+
+
+from pytest import mark, param
+
+from iterative_comb_sort import iter_comb_sort as isort
+
+
+# pylint: disable=arguments-out-of-order
+
+
+@mark.parametrize("seq, res", [([], []), ("", []), ((), [])])
+def test_empty_sequences(seq, res):
+    """Empty sequences."""
+    assert isort(seq) == res
+
+
+@mark.parametrize("seq", [[1], "1", (1,)])
+def test_single_element_sequence(seq):
+    """Single element sequences."""
+    res = list(seq)
+    assert isort(seq) == res
+
+
+@mark.parametrize("seq", [[-1, 1], (-2, 0, 2),
+                          "ABCxyz", "1234567",
+                          ([1, 2], [2, 3], [5, 7, 9])])
+def test_sorted_sequences(seq):
+    """Sorted sequences."""
+    res = list(seq)
+    assert isort(seq) == res
+
+
+@mark.parametrize("seq", [[1, 0], (2, -2, 0),
+                          "qwerty", "School42",
+                          ([1, -1], [5, 0, 15, -10])])
+def test_unsorted_sequences(seq):
+    """Unsorted sequences."""
+    res = sorted(seq)
+    assert isort(seq) == res
+
+
+@mark.parametrize(
+    "nonseq", [param(42, marks=mark.xfail), param(None, marks=mark.xfail)]
+)
+def test_nonsequences(nonseq):
+    """Non-sequences."""
+    assert isort(nonseq)
